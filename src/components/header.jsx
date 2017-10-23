88c2236617bdd5074592b6a28c678ae5bfe4b651
Teddy Radenkov <teddy.pdx@gmail.com>
2017-10-23 00:42:11 -0700

    Added markup and styles for header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -31,6 +31,26 @@ export default class Header extends React.Component {
 					<div class={'menu-bar middle ' + this.state.menuState}></div>
 					<div class={'menu-bar bottom ' + this.state.menuState}></div>
 				</div>
+				<main>
+					<div id="title">
+						<div class="anchor top-left"></div>
+						<div class="anchor top-right"></div>
+						<div class="anchor bottom-right"></div>
+						<div class="anchor bottom-left"></div>
+						<h1><span>Teddy </span><span class="highlight">Radenkov</span></h1>						
+					</div>
+					<div id="tag-line">
+						<h2><span class="highlight">Front-End</span> Engineer</h2>
+						<div id="line-breaker"></div>
+						<h2>Currently available for work</h2>
+					</div>
+					<div id="learn-more">
+						<h3>Learn <span class="highlight"> more </span> about me</h3>
+						<div id="scroll-icon-mouse">
+							<div id="scroll-icon-button"></div>
+						</div>
+					</div>
+				</main>
 			</header>
 		);
 	}
