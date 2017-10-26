b540be778ec6b571d85c0804d3ca74d721b2dc96
Teddy Radenkov <teddy.pdx@gmail.com>
2017-10-26 00:18:22 -0700

    Added menu styles and animatons for mobile

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,28 +2,13 @@
 import React from 'react';
 import MediaQuery from 'react-responsive';
 
+//Components
+import MobileMenu from 'mobileMenu.jsx';
+
 export default class Header extends React.Component {
 	constructor(props){
 		super(props)
-		this.state = {menuState: ''};	
-		this.hoverOnMenuIcon = this.hoverOnMenuIcon.bind(this);
-		this.hoverOffMenuIcon = this.hoverOffMenuIcon.bind(this);
-		this.tagline = this.tagline.bind(this);		
-	}
-	componentDidMount(){
-		this.menuIcon = document.getElementById('menu-icon');
-		this.menuIcon.addEventListener('mouseover', this.hoverOnMenuIcon, false);
-		this.menuIcon.addEventListener('mouseout', this.hoverOffMenuIcon, false);			
-	}
-	componentWillUnmount(){
-		this.menuIcon.removeEventListener('mouseover', this.hoverOnMenuIcon, false);
-		this.menuIcon.removeEventListener('mouseout', this.hoverOffMenuIcon, false);	
-	}
-	hoverOnMenuIcon(){
-		this.setState({menuState: 'hovered'});
-	}
-	hoverOffMenuIcon(){
-		this.setState({menuState: ''});
+		this.tagline = this.tagline.bind(this);	
 	}
 	tagline(){
 		return (
@@ -37,11 +22,22 @@ export default class Header extends React.Component {
 	render(){
 		return (
 			<header>
-				<div id='menu-icon' data-aos='fade-right'>
-					<div class={'menu-bar top ' + this.state.menuState}></div>
-					<div class={'menu-bar middle ' + this.state.menuState}></div>
-					<div class={'menu-bar bottom ' + this.state.menuState}></div>
-				</div>
+				<MediaQuery query='(max-width: 1023px)'>
+					<MobileMenu/>		
+				</MediaQuery>
+				<MediaQuery query='(min-width: 1024px)'>
+					<div id='menu'>
+						<nav>
+							<ul>
+								<li data-aos='fade-down'><span>Expertise</span></li>
+								<li data-aos='fade-down' data-aos-delay='200'><span>Skills</span></li>
+								<li data-aos='fade-down' data-aos-delay='400'><span>Work</span></li>
+								<li data-aos='fade-down' data-aos-delay='800'><span>About</span></li>
+								<li data-aos='fade-down' data-aos-delay='1000'><span>Get In Touch</span></li>
+							</ul>
+						</nav>
+					</div>	
+				</MediaQuery>					
 				<main>
 					<div id='title' data-aos='fade' data-aos-delay='500'>
 						<div class='anchor top-left' data-aos='fade-down-right' data-aos-delay='1000'></div>
@@ -59,7 +55,7 @@ export default class Header extends React.Component {
 						{(matches) => { return matches ? this.tagline() : null }}
 					</MediaQuery>
 				</main>
-				<div id='learn-more' data-aos='fade-up' data-aos-delay='2500' data-aos-anchor='#title'>
+				<div id='learn-more' data-aos='fade-up' data-aos-delay='2000' data-aos-anchor='#title'>
 					<h3>Learn <span class='highlight'> more </span> about me</h3>
 					<div id='scroll-icon-mouse'>
 						<div id='scroll-icon-button'></div>
---
+++ src/components/mobileMenu.jsx
@@ -0,0 +1,59 @@
+//React modules
+import React from 'react';
+import MediaQuery from 'react-responsive';
+
+export default class MobileMenu extends React.Component {
+	constructor(props){
+		super(props)
+		this.state = {hover: false, open: false};	
+		this.menuHover = this.menuHover.bind(this);
+		this.menuOpen = this.menuOpen.bind(this);
+		this.menuList = this.menuList.bind(this);
+	}
+	componentDidMount(){
+		this.menuIcon = document.getElementById('menu-icon');
+		this.menuIcon.addEventListener('mouseover', this.menuHover, false);
+		this.menuIcon.addEventListener('mouseout', this.menuHover, false);
+		this.menuIcon.addEventListener('click', this.menuOpen, false);			
+	}
+	componentWillUnmount(){
+		this.menuIcon.removeEventListener('mouseover', this.menuHover, false);
+		this.menuIcon.removeEventListener('mouseout', this.menuHover, false);
+		this.menuIcon.removeEventListener('click', this.menuOpen, false);	
+	}
+	menuHover(){
+		this.setState({hover: !this.state.hover});
+	}
+	menuOpen(){
+		this.setState({open: !this.state.open});
+	}
+	menuList(animate){
+		if(animate){
+			return (
+				<nav data-aos='fade'>
+					<ul>
+						<li data-aos='fade-right' data-aos-delay='600'>Expertise</li>
+						<li data-aos='fade-right' data-aos-delay='800'>Skills</li>
+						<li data-aos='fade' data-aos-delay='1000'>Work</li>
+						<li data-aos='fade-left' data-aos-delay='800'>About</li>
+						<li data-aos='fade-left' data-aos-delay='600'>Get In Touch</li>
+					</ul>
+				</nav>
+			);
+		}
+	}
+	render(){
+		let hover = this.state.hover ? ' hover' : '';
+		let open = this.state.open ? ' open' : '';
+		return (
+			<div id="menu-mobile">		
+				{this.menuList(this.state.open)}	
+				<div id='menu-icon' data-aos='fade-right'>
+					<div class={'menu-bar top' + hover + open}></div>
+					<div class={'menu-bar middle' + hover + open}></div>
+					<div class={'menu-bar bottom' + hover + open}></div>
+				</div>
+			</div>	
+		);
+	}
+}
