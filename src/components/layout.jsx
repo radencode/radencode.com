15c027f7f0ed59d4e34a241a2f1b2ddefcb4ef7d
Teddy Radenkov <teddy.pdx@gmail.com>
2017-11-09 23:36:13 -0800

    Added skills section

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from 'header/header.jsx';
 import Expertise from 'expertise/expertise.jsx';
+import Skills from 'skills/skills.jsx';
 
 export default class Layout extends React.Component {
 	render(){
@@ -8,6 +9,7 @@ export default class Layout extends React.Component {
 			<div class='layout'>
 				<Header/>
 				<Expertise/>
+				<Skills/>
 			</div>
 		);
 	}
