ce4a33f8505a87d106af1ea4cc4ecdad29ed5d49
Teddy Radenkov <teddy.pdx@gmail.com>
2017-11-09 23:37:38 -0800

    Add translate feature for the target component

--- src/components/global/target.jsx
+++ src/components/global/target.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 export default class Target extends React.PureComponent{
   render(){
     return (
-      <div class={'target ' + this.props.shape} style={this.props.position}>
-        <div class='target-area outer' data-aos='fade-right'></div>
-        <div class='target-area outer-middle' data-aos='fade-left' data-aos-delay={200}></div>
-        <div class='target-area inner-middle' data-aos='fade-right' data-aos-delay={400}></div>
-        <div class='target-area inner' data-aos='fade-left' data-aos-delay={600}></div>
+      <div class={this.props.translate ? 'target translate ' + this.props.shape : 'target ' + this.props.shape} style={this.props.position}>
+        <div class={'target-area ' + this.props.color + ' outer'} data-aos='fade-right'></div>
+        <div class={'target-area ' + this.props.color + ' outer-middle'} data-aos='fade-left' data-aos-delay={200}></div>
+        <div class={'target-area ' + this.props.color + ' inner-middle'} data-aos='fade-right' data-aos-delay={400}></div>
+        <div class={'target-area ' + this.props.color + ' inner'} data-aos='fade-left' data-aos-delay={600}></div>
       </div>
     );
   }
