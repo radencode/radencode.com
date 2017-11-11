840274026ab9a84ce3abd7455801989d094ad728
Teddy Radenkov <teddy.pdx@gmail.com>
2017-11-11 00:28:09 -0800

    Allowed target component to be dynamically styled

--- src/components/global/target.jsx
+++ src/components/global/target.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Target extends React.PureComponent{
   render(){
     return (
-      <div class={this.props.translate ? 'target translate ' + this.props.shape : 'target ' + this.props.shape} style={this.props.position}>
+      <div class={this.props.translate ? 'target translate ' + this.props.shape : 'target ' + this.props.shape}>
         <div class={'target-area ' + this.props.color + ' outer'} data-aos='fade-right'></div>
         <div class={'target-area ' + this.props.color + ' outer-middle'} data-aos='fade-left' data-aos-delay={200}></div>
         <div class={'target-area ' + this.props.color + ' inner-middle'} data-aos='fade-right' data-aos-delay={400}></div>
