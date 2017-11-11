bcabaf40dfdad011d453954d7ebc8e308dc52bbd
Teddy Radenkov <teddy.pdx@gmail.com>
2017-11-11 00:29:49 -0800

    Added particles to background and square component

--- src/components/skills/skills.jsx
+++ src/components/skills/skills.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import MediaQuery from 'react-responsive';
+import Particles from 'react-particles-js';
 import Skillset from 'skills/skillset.jsx';
 import Title from 'global/title.jsx';
 import Square from 'global/square.jsx';
 import Discription from 'global/discription.jsx';
+import particlesConfig from 'particles-config-expertise.json';
 
 export default class Skills extends React.Component{
   componentWillMount(){
@@ -153,11 +156,19 @@ export default class Skills extends React.Component{
   render(){    
     return (
       <section id='skills'>
+				<MediaQuery query='(min-width: 1024px)'>
+          <Particles params={particlesConfig} style={{width: '100%', height: '100%', position: 'absolute', top: '0px', left: '0px'}}/>		
+				</MediaQuery>
 				<Title title='Skills' subTitle={'I always strive for the better'}/>
 				<Discription id='skills-anchor' discription='I thrive in a challenging environment that requires quick thinking and problem solving skills. I love to learn and continue to practice with the latest technology in hopes of achieving my personal goals as well as better my skills.'/>
-        <Skillset skills={this.frontEndSkills}/>
-        <Skillset skills={this.backEndSkills}/>
-        <Skillset skills={this.devToolSkills}/>
+        <div id='skill-path'>
+					<MediaQuery query='(min-width: 1600px)'>
+						<Square size='big' angle='forty-five' position={{top: '717px', left: '-188px'}}/>		
+					</MediaQuery>
+					<Skillset skills={this.frontEndSkills}/>
+					<Skillset skills={this.backEndSkills}/>
+					<Skillset skills={this.devToolSkills}/>
+				</div>
       </section>
     );
   }    
