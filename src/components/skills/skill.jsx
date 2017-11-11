399363b535b278d12262e2778f2822f0305273c5
Teddy Radenkov <teddy.pdx@gmail.com>
2017-11-11 00:29:17 -0800

    Changed animation for the entire skill instead of individual parts of it.

--- src/components/skills/skill.jsx
+++ src/components/skills/skill.jsx
@@ -5,7 +5,7 @@ import Anchor from 'global/anchor.jsx';
 export default class Skill extends React.PureComponent{
   render(){    
     return (
-      <div id={'skill-' + this.props.skill.id} class='skill'>
+      <div id={'skill-' + this.props.skill.id} class='skill' data-aos={this.props.animation.name} data-aos-delay={this.props.animation.delay} data-aos-anchor={this.props.animation.anchor}>
 				<MediaQuery query='(min-width: 1024px)'>
 					<Anchor type='top-left' color='dark-blue' animation='fade-down-right' delay='0' anchor={"#skill-" + this.props.skill.id}/>
 					<Anchor type='top-right' color='dark-blue' animation='fade-down-left' delay='0' anchor={"#skill-" + this.props.skill.id}/>
@@ -13,14 +13,14 @@ export default class Skill extends React.PureComponent{
 					<Anchor type='bottom-left' color='dark-blue' animation='fade-up-right' delay='0' anchor={"#skill-" + this.props.skill.id}/>
         </MediaQuery>
         <div class='skill-properties' >
-          <div class={'skill-diamond ' + this.props.color} data-aos='fade-left'>
+          <div class={'skill-diamond ' + this.props.color}>
             <div class='skill-timeframe'>
               <h2 class='skill-years'>{this.props.skill.years}</h2>
               <h2 class='skill-tag'>{this.props.skill.tag}</h2>
             </div>
           </div>
-          <h2 class={'skill-name ' + this.props.color} data-aos='fade-right'>{this.props.skill.name}</h2>
-          <div class='skill-progress' data-aos='fade'>
+          <h2 class={'skill-name ' + this.props.color} >{this.props.skill.name}</h2>
+          <div class='skill-progress'>
             <div class={'skill-level ' + this.props.color} style={{width: this.props.skill.skilllevel}}></div>
           </div>
         </div>
