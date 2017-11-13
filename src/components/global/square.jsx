3cb17feeabff88a5cead3ea81f535733306edf80
Teddy Radenkov <teddy.pdx@gmail.com>
2017-11-12 21:28:53 -0800

    Allowed option for a lighter color

--- src/components/global/square.jsx
+++ src/components/global/square.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Square extends React.PureComponent{
   render(){
     return (
-			<div class={'square ' + this.props.size + ' ' + this.props.angle} style={this.props.position}></div>      
+			<div class={this.props.light ? 'square light ' + this.props.size + ' ' + this.props.angle : 'square ' + this.props.size + ' ' + this.props.angle} style={this.props.position}></div>      
     );
   }
 }
