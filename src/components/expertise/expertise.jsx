d3904e080503d9675e7ac12d0232feebb0ad6ef3
Teddy Radenkov <teddy.pdx@gmail.com>
2017-11-12 21:28:40 -0800

    Allowed option for a lighter color

--- src/components/expertise/expertise.jsx
+++ src/components/expertise/expertise.jsx
@@ -48,8 +48,8 @@ export default class Expertise extends React.Component{
           <Particles params={particlesConfig} style={{width: '100%', height: '100%', position: 'absolute', top: '0px', left: '0px'}}/>		
 				</MediaQuery>        
         <MediaQuery query='(min-width: 1600px)'>
-          <Square size='big' angle='thirty-right' position={{top: '800px', right: '-1000px'}}/>
-					<Square size='big' angle='fifteen-left' position={{top: '400px', left: '-900px'}}/>		
+          <Square light={false} size='big' angle='thirty-right' position={{top: '800px', right: '-1000px'}}/>
+					<Square light={false} size='big' angle='fifteen-left' position={{top: '400px', left: '-900px'}}/>		
 				</MediaQuery>
 				<Title title='Expertise' subTitle={'This is how I create & build'}/>
 				<Discription id='expertise-anchor' discription='I love to create eye catching and easy to use applications that provide a great user experience. I specialize in developing with React.js/Redux and RESTful APIs as a backend to my applications. Developing web applications is more than a job to me; it is a hobby. Although I always strive for the end product with customer satisfaction at highest priority, it is the collection of experiences gained over time that drives me. Oh... and did I mention I love JavaScript?!'/>
